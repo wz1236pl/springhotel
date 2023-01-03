b49db30fb35d813f71696c3fc2987fa8550af99e
Lukasz <dziq243@gamil.com>
2023-01-03 12:53:00 +0100

    import delete, springboot 2.7.6 -> 2.7.7

--- src/main/java/springboot/hotele/Kontrolery.java
+++ src/main/java/springboot/hotele/Kontrolery.java
@@ -1,7 +1,5 @@
 package springboot.hotele;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
